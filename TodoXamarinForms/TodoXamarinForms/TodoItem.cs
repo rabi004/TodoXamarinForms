1d04fa511d08b9062d9ed68b008b46a2c369d1b2
rabi alam <rabi_004@hotmail.com>
2019-10-20 03:12:36 -0500

    initial commit

---
+++ TodoXamarinForms/TodoXamarinForms/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoXamarinForms
+{
+    public class TodoItem : BaseFodyObservable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
+
---
+++ TodoXamarinForms/TodoXamarinForms/TodoListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Collections.ObjectModel;
+
+namespace TodoXamarinForms
+{
+    public class TodoListViewModel : BaseFodyObservable
+    {
+        public string Title => "My Todo List";
+        public ObservableCollection<TodoItem> TodoList { get; set; } = new ObservableCollection<TodoItem>
+{
+    new TodoItem { Id = 0, Title = "Create First Todo", IsCompleted = true},
+    new TodoItem { Id = 1, Title = "Run a Marathon"},
+    new TodoItem { Id = 2, Title = "Create TodoXamarinForms blog post"},
+};
+    }
+}
